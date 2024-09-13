0be529cbfed1c552f2d661dfd3112926d81f7113
stigward <jmaginnes@gmail.com>
2024-09-13 13:10:40 -0600

    removed default tag view

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -19,7 +19,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
-    Component.TagList(),
   ],
   left: [
     Component.PageTitle(),
