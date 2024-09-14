417973021eebc874c70413b39ab23a9c5dd496df
stigward <jmaginnes@gmail.com>
2024-09-14 07:48:55 -0600

    added google analytics

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,7 +12,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: 'google',
+      tagId: 'G-KBEJ39RLZZ'
     },
     locale: "en-US",
     baseUrl: "bug.directory",
